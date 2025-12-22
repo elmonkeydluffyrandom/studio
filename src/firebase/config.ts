e2516cd5e8691856ec95f1bf253847002b0e48fe
Luffy D <elmonkeyd.luffyrandom@gmail.com>
2025-12-22 04:59:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1263362726-21d34",
+  "appId": "1:909834642582:web:226d09d95a4b0c35ab4eb7",
+  "apiKey": "AIzaSyDIvra58g38A7q_NakyM2X9BmLtjY0zLbI",
+  "authDomain": "studio-1263362726-21d34.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "909834642582"
+};
